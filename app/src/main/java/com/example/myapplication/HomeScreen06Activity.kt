1846a119f923175216d7399fe4d47189b69811ff
Tanisha <tanishasuhag127@gmail.com>
2025-04-23 20:47:53 +0530

    Fixed profile icon navigation

--- app/src/main/java/com/example/myapplication/HomeScreen06Activity.kt
+++ app/src/main/java/com/example/myapplication/HomeScreen06Activity.kt
@@ -3,19 +3,17 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.LinearLayout
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-class HomeScreen01Activity : AppCompatActivity() {
+class HomeScreen06Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_screen_01) // link to home_screen_01.xml
+        setContentView(R.layout.home_screen_06) // link to home_screen_06.xml
 
         // Connect the profile icon click to BuyerProfileActivity
         val navProfile = findViewById<LinearLayout>(R.id.navProfile)
         navProfile.setOnClickListener {
-            Log.d("ProfileClick", "Profile icon clicked")
             val intent = Intent(this, BuyerProfileActivity::class.java)
             startActivity(intent)
         }
